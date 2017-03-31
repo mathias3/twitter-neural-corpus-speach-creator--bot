bb8799c868139f04bf96b5c3f5e0d762dd8fbdfc
mathias3 <maciej.osowski@gmail.com>
2017-03-31 01:17:10 +0100

    Update strings.js

--- src/helpers/strings.js
+++ src/helpers/strings.js
@@ -1,12 +1,37 @@
 module.exports = {
   queryString: [
-    ''
+    'Data Science',
+    '#DataViz',
+    'deep learning',
+    'machine learning',
+    'kaggle',
+    'convolutional network',
+    'augmented reality',
+    'neural netwotk',
+    'artificial intelligence',
+    'data mining'
   ],
   queryStringSubQuery: [
     ''
+    
   ],
   blockedStrings: [
-    ''
+        'porn',
+        'sex',
+        'emotions',
+        'fuck',
+        'gay',
+        'erotic',
+        'idiot',
+        'anal',
+        'ban',
+        'trump',
+        'fight',
+        'politic',
+        'shit',
+        'imigrant',
+        'milf',
+        'booty'
   ],
   resultType: [
     'mixed',
@@ -14,6 +39,13 @@ module.exports = {
     'popular'
   ],
   responseString: [
-    'Hi @screenName thanks for the following!'
+        'Hi @screenName thanks for the follow! Hope you are allright ',
+        '@screenName thanks for following! Grettings!',
+        'Hey @screenName thanks for the follow! Mind the step! ',
+        'Thanks for following @screenName! Havea good one ',
+        'Hey @screenName, thanks for following! Hope you will be satisfied with content ',
+        'Awesome @screenName, thanks for following! Keep it up ',
+        'Thanks for the follow @screenName! Have a great day!',
+        'Thanks for following @screenName! How are you today? '
   ]
 }
